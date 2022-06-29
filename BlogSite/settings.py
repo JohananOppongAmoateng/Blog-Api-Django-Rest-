7b713e69016048a2ffe41f1f54c8844ff6004f29
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-06-29 13:33:59 +0000

    blog api with comments

--- BlogSite/settings.py
+++ BlogSite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&*se_x3g^+@a94!2-1w+btl&ris05ac4ie^%@rt*t=_ib5q5b0'
+SECRET_KEY = os.getenv(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv(DEBUG)
 
 ALLOWED_HOSTS = []
 
